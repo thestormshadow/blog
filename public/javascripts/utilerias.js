3fc1b0ddf24d75893a270028a027cf6c1aebd345
thestormshadow <javiervargasruiz94@gmail.com>
2016-09-26 00:35:08 -0500

    old version
    new express v4

--- public/javascripts/utilerias.js
+++
@@ -1,7 +0,0 @@
-function disablethis(item){
-                if(document.getElementById(""+item).style.display == "none"){
-                    $("#"+item).fadeIn("slow");
-                }else{
-                    $("#"+item).fadeOut("slow");
-                }
-            }
