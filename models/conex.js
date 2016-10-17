01586ac69f97c0c5d9553b752ff1b0578b852e5e
thestormshadow <javiervargasruiz94@gmail.com>
2016-10-17 15:23:51 -0500

    Update conex.js

--- models/conex.js
+++ models/conex.js
@@ -3,8 +3,8 @@ var mongodb = require('mongodb'),
     MongoClient = mongodb.MongoClient;
 //var db;
 var BLOG = {};
-MongoClient.connect("mongodb://localhost:27017/blogjvr94", function(err, database) {
-//MongoClient.connect("mongodb://wwwcodeingenius:25661483@waffle.modulusmongo.net:27017/r6asEmen", function(err, database) {
+//MongoClient.connect("mongodb://localhost:27017/blogjvr94", function(err, database) {
+MongoClient.connect("mongodb://wwwcodeingenius:25661483@waffle.modulusmongo.net:27017/r6asEmen", function(err, database) {
     if(err) throw err;
     BLOG.db = database;
     BLOG.posts = database.collection('posts');
